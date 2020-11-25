3973cc998bad291716eac8f814bc1ab7ea8afb51
Alexander Kaiser <alexander.kaiser@stud.hs-flensburg.de>
2020-11-25 13:50:07 +0100

    initial branch commit

--- src/Application.kt
+++ src/Application.kt
@@ -6,17 +6,44 @@ import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.sessions.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+data class Student(val name: String, val matriculationNumber: Number)
+data class Students(val value: List<Student>)
 
 //@Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    val server = embeddedServer(Netty, port = 8080) {
-        routing {
-            get ("/") {
-                call.respondText("Hello World!")
+    install(Sessions) {
+        cookie<Students>("SESSION")
+    }
+    routing {
+        get ("/") {
+            val headers = call.request.headers
+            var html = ""
+
+            call.sessions.set(
+                Students(listOf(
+                    Student("Alexander Kaiser", 123456),
+                    Student("Andre Müller", 456789),
+                    Student("Tilman Haß", 987654),
+                    Student("Torge Jensen", 100000),
+
+                ))
+            )
+
+//            call.sessions.set(Students(listOf("sdaf", "sdaf", "sdaf")))
+
+            headers.entries().forEach {
+                html += "${it.key}: ${it.value}\n"
             }
+            call.respondText(html)
+        }
+        get ("/cookies") {
+            val get = call.sessions.get<Students>()
+            call.respondText(get?.value?.get(0)?.name.orEmpty())
         }
     }
 }
