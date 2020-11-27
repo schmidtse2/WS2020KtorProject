ee36c18beb329132c2df746af7b5b2966d12beb4
Alexander Kaiser <alexander.kaiser@stud.hs-flensburg.de>
2020-11-27 17:27:24 +0100

    bla damit das im git ist

--- src/Application.kt
+++ src/Application.kt
@@ -10,36 +10,23 @@ import io.ktor.sessions.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-data class Students(val value: List<Number>)
+data class Students(val matriculationNumber: List<Number>)
 
-//@Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(Sessions) {
         cookie<Students>("SESSION")
     }
     routing {
-        get ("/") {
-            val headers = call.request.headers
-            var html = ""
-
-            call.sessions.set(
-                Students(listOf(
-                    123456,
-                    456789,
-                    987654,
-                    100000,
-                ))
-            )
-
-            headers.entries().forEach {
-                html += "${it.key}: ${it.value}\n"
-            }
-            call.respondText(html)
+        get("/") {
+            call.sessions.set(Students(
+                    listOf(123, 1654, 378923)
+            ))
+            call.respond("Session Beispiel")
         }
-        get ("/cookies") {
+        get("/session") {
             val session = call.sessions.get<Students>()
-            call.respondText(session?.value.toString())
+            call.respondText(session?.matriculationNumber.toString())
         }
     }
 }
