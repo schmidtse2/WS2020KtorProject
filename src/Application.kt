cb362118a9863101e0a203c3fc026142d185b732
Andre Müller <andre.mueller20@web.de>
2020-11-28 23:24:24 +0100

    Modules und Autoreload implementiert

--- src/Application.kt
+++ src/Application.kt
@@ -7,20 +7,40 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+//fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 //@Suppress("unused") // Referenced in application.conf
 //@kotlin.jvm.JvmOverloads
+
+
+fun main(args: Array<String>){
+    val server = embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
+}
+
 
 fun Application.module() {
     install(CallLogging)
     routing {
         get ("/hello") {
-            call.respondText("Hellooo Worlds!")
+            call.respondText("Hello !")
         }
         get ("/bye"){
-            call.respondText("Goodbye miserable World!")
+            call.respondText("Goodbye !")
         }
+    }
+}
+
+fun Application.myModule(){
+    routing {
+        get("/test"){
+            call.respondText { "This test." }
+        }
+    }
+}
+
+fun Routing.root(){
+    get("/"){
+        call.respondText("Sweet Home.")
     }
 }
 
